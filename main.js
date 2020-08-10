975185a47d76009b9a46c939c17b2da573007aff
shugo9538 <shugo9538@gmail.com>
2020-08-10 15:37:50 +0900

    finish book

--- main.js
+++ main.js
@@ -23,7 +23,8 @@ mongoose.Promise = global.Promise;
 
 if (process.env.NODE_ENV === "test") {
   mongoose.connect(
-    "mongodb+srv://shugo9538:2tmdwn8247-@cluster0.9inua.mongodb.net/recipe_test_db", {
+    "mongodb://localhost:27017/recipe_db_test", {
+    // "mongodb+srv://shugo9538:2tmdwn8247-@cluster0.9inua.mongodb.net/recipe_test_db", {
       useNewUrlParser: true,
       useFindAndModify: false
     }
--- test/usersControllerSpec.js
+++ test/usersControllerSpec.js
@@ -17,31 +17,8 @@ describe("usersController", () => {
         .end((errors, res) => {
           expect(res).to.have.status(200);
           expect(errors).to.be.equal(null);
-          done();
         });
-    });
-  });
-
-  describe("getUserParams", () => {
-    it("should convert request body to contain the name attributes of the user object", () => {
-      var body = {
-        first: "Jon",
-        last: "Wexler",
-        email: "jon@jonwexler.com",
-        password: 12345,
-        zipCode: 10016
-      };
-      expect(usersController.getUserParams(body)).to.deep.include({
-        name: {
-          first: "Jon",
-          last: "Wexler"
-        }
-      });
-    });
-
-    it("should return an empty object with empty request body input", () => {
-      var emptyBody = {};
-      expect(usersController.getUserParams(emptyBody)).to.deep.include({});
+        setTimeout(done, 300);
     });
   });
 });
